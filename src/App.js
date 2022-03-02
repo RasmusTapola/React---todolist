a03ee168c4ac829ac890fdd3d0ee593b687eec75
Rasmus <rasmus.tapola00@gmail.com>
2022-03-02 12:39:36 +0200

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [todo, setTodo] = useState({ desc: '', date: '' });
+    const [todos, setTodos] = useState([]);
+   
+
+    const inputChanged = (event) => {
+        setTodo({...todo, [event.target.name]:event.target.value });
+    }
+
+    const addTodoList = (event) => {
+        event.preventDefault();
+        setTodos([...todos, todo]);
+    }
+
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <form onSubmit={addTodoList}>
+
+                Description: <input type="text" name="desc" value={todo.desc} onChange={inputChanged}></input>
+                Date: <input type="date" name="date" value={todo.date} onChange={inputChanged}></input>
+                    <input type="submit"></input>
+            </form>
+            
+            <table>
+                <tbody>
+                    
+                    <th>Description:</th>
+                    <th>Date:</th>
+                    
+                    
+                    {
+                    todos.map((todo, index) =>
+                        <tr key={index}>                           
+                            <td>{todo.desc}</td>                           
+                            <td>{todo.date}</td>    
+                    </tr>
+                        )
+                    }
+                </tbody>
+          </table>
     </div>
   );
 }
